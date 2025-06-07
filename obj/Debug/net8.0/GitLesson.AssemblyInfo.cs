eafdf53d871ec3cd6aec91c90039621b1eff7c1f
Svt Dev <SvtDev@gmail.com>
2025-06-07 21:38:38 +0300

    Second MergeConflict.cs

--- obj/Debug/net8.0/GitLesson.AssemblyInfo.cs
+++ obj/Debug/net8.0/GitLesson.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GitLesson")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc0fd9a00069d0c2864e20b468222a1e93866ff8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c77d3df765b02965894cf058a523ee1afba16486")]
 [assembly: System.Reflection.AssemblyProductAttribute("GitLesson")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GitLesson")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
